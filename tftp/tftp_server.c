b0b65f0206c3fff4e4e8d3171ed65b6887df45e5
wangli.qufu <wangli.qufu@163.com>
2017-08-02 01:30:50 +0800

    add RRQ handle buf can't open file

--- tftp/tftp_server.c
+++ tftp/tftp_server.c
@@ -12,6 +12,108 @@
 #define ACK 4
 #define ERROR 5
 
+
+#define BUF_SIZE 600
+char send_buf[BUF_SIZE];
+void send_error(int error_code,char *error_msg)
+{
+	
+}
+int make_data(char *dst_buf,short int block,char *src_data_buf,int data_len)
+{
+	int index = 0;	
+	short opcode = htons(DATA);	
+	memcpy(dst_buf+index,&opcode,sizeof(opcode));
+	index += 2;
+
+	short int net_block = htons(block);
+	memcpy(dst_buf+index,&net_block,sizeof(net_block));
+	index += 2;
+
+	memcpy(dst_buf+index,src_data_buf,data_len);
+	index += data_len;
+
+	return index;
+}
+int handle_RRQ(char *in_buf,char *out_buf)
+{
+	char file_name[512] = {0};
+	char mode[512] = {0};
+	char *pIndex = &in_buf[1];
+	char *pFile = file_name;
+	char *pMode = mode;
+	while(*pIndex != '\0')
+	{
+		*pFile++ = *pIndex++;	
+	}
+	pIndex++;
+	while(*pIndex != '\0')
+	{
+		*pMode++ = *pIndex++;
+	}
+
+	printf("RRQ file_name: %s\n",file_name);
+	printf("RRQ mode: %s\n",mode);
+
+	FILE *file = fopen(file_name,"r");
+	if(NULL == file)
+	{
+		// TODO:send error	
+		printf("open %s error,cwd = %s\n",file_name,get_current_dir_name());
+		exit(0);
+	}
+
+	char buf[512] = {0};
+	int read_len = fread(buf, 512, 1, file);
+	int len = make_data(out_buf, 0, buf, read_len);
+	fclose(file);
+	
+	return len;
+}
+
+int handle_WRQ(char *in_buf,char *out_buf)
+{
+	printf("WRQ: %s\n",&in_buf[1]);
+	int index = 0;
+	return index;
+}
+int handle_req(char *in_buf,char *out_buf)
+{
+	int index = 0;
+	unsigned short req = ntohs(*(unsigned short *)&in_buf[0]);
+	switch(req)
+	{
+	case RRQ:
+		index = handle_RRQ(in_buf,out_buf);	
+		break;
+	case WRQ:
+		index = handle_WRQ(in_buf,out_buf);
+		break;
+	case DATA:
+		{
+			short int seq=htons(*(unsigned short int*)&in_buf[1]);
+			printf("DATA: %d\n",seq);
+			break;
+		}
+	case ACK:
+		{
+			short seq=htons(*(unsigned short *)&in_buf[1]);
+			printf("ACK: %d\n",seq);
+			break;
+		}
+	case ERROR:
+		{
+			unsigned short errcode=htons(*(unsigned short *)&in_buf[1]);
+			printf("ERROR: %d %s\n",errcode,&in_buf[3]);
+			break;
+		}
+		
+	default:
+		printf("unkown request: %d\n",req);
+	}
+	return index;
+
+}
 int main(int argc,char** argv)
 {
     struct sockaddr_in serveraddr;
@@ -38,17 +140,20 @@ int main(int argc,char** argv)
 
     struct sockaddr_in cliaddr = serveraddr;
     socklen_t len = sizeof(cliaddr);
-    char buf[512];
+    char in_buf[512];
+    char out_buf[600];
     while(1)
     {
-        memset(buf,0,sizeof(buf));
-        int errno = recvfrom(serverfd,buf,sizeof(buf),0,(struct sockaddr*)&cliaddr,&len);
+        memset(in_buf,0,sizeof(in_buf));
+        memset(out_buf,0,sizeof(out_buf));
+        int errno = recvfrom(serverfd,in_buf,sizeof(in_buf),0,(struct sockaddr*)&cliaddr,&len);
         if(errno <= 0)
         {
              perror("recvfrom error");
         }
-        printf("recv msg: %s",buf);
-        sendto(serverfd,buf,strlen(buf),0,(struct sockaddr*)&cliaddr,len);
+        //printf("recv msg: %s",buf);
+		int out_len = handle_req(in_buf,out_buf);
+        sendto(serverfd,out_buf,out_len,0,(struct sockaddr*)&cliaddr,len);
     }
 
     return 0;
